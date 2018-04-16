ccc161c8bb23ecd9a4105e4e46a828fd5d22c1b7
dfggking <835317619@qq.com>
2018-04-16 21:09:48 +0800

    此项目用于测试最新JavaWEB技术和方法

---
+++ src/main/java/com.web.controller/TestController.java
@@ -0,0 +1,14 @@
+package com.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author dfggking@hotmail.com
+ * @version 1.0
+ * @date 2018/4/16
+ */
+@Controller
+public class TestController {
+
+
+}
